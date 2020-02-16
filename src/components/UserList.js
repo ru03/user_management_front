efd10717e2e2b22762ae471d4ed2582e68ee7b72
ru03 <ruben.lmangas@gmail.com>
2020-02-16 16:05:13 +0100

    Add delete user

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -12,15 +12,16 @@ import {
   TablePagination,
   TableRow,
 } from '@material-ui/core/';
-import { Check, Clear } from '@material-ui/icons';
+import { Check, Clear, Delete } from '@material-ui/icons';
 import { Spinner } from './UI';
 import moment from 'moment';
 import env from '../config/config';
 
 const UserList = () => {
-  const COLUMS = ['Name', 'Last Name', 'Email', 'Active', 'Updated At'];
+  const COLUMS = ['Name', 'Last Name', 'Email', 'Active', 'Updated At', 'Delete'];
   const [pagination, setPagination] = useState({ page: 0, order: 'DESC', limit: 10 });
   const { data, isLoading, sendRequest } = useFetch();
+  const { isLoading: isDeleteLoading, sendRequest: sendDeleteRequest } = useFetch();
 
   useEffect(() => {
     const { page, order, limit } = pagination;
@@ -31,10 +32,20 @@ const UserList = () => {
       }
     }
     sendRequest(`${env.basepath}${env.users}?page=${page}&limit=${limit}&order=${order}`, config);
-  }, [pagination, sendRequest]);
+  }, [isDeleteLoading, pagination, sendRequest]);
 
   const handleChangePage = (_, newPage) => {
     setPagination({ ...pagination, page: newPage });
+  }
+
+  const deleteUser = (id) => {
+    const config = {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+    sendDeleteRequest(`${env.basepath}${env.users}/${id}`, config);
   }
 
   return (
@@ -42,7 +53,7 @@ const UserList = () => {
       <Box mt='20px'>
         <TableContainer component={Paper} mt={5}>
           {
-            isLoading
+            isLoading || isDeleteLoading
               ? <Spinner />
               : (
                 <Table>
@@ -62,6 +73,7 @@ const UserList = () => {
                             {res.active ? <Check /> : <Clear />}
                           </TableCell>
                           <TableCell>{moment(res.updatedAt).format('DD/MM/YYYY HH:mm:ss')}</TableCell>
+                          <TableCell><Delete onClick={() => deleteUser(res.id)} /></TableCell>
                         </TableRow>
                       ))
                     }

0e119bc7c8c720549376bb3693c53097f72f116c
ru03 <ruben.lmangas@gmail.com>
2020-02-25 23:44:00 +0100

    fix env config

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 import dev from './dev';
 import prod from './prod';
 
-const config = process.env.NODE_ENV !== ('prod' || 'production') ? dev : prod;
+const config = process.env.NODE_ENV !== 'production' ? dev : prod;
 
 export default config;
